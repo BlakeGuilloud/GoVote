6c6c7effb0b0abdb93820eb7bcaf2e304978f9da
Blake Guilloud <blake.guilloud@gmail.com>
2018-03-19 19:54:28 -0400

    Blake - docker POC.

--- server/server.js
+++ server/server.js
@@ -1,21 +1,22 @@
 /* eslint no-console: 0 */
 import express from 'express';
-import pg, { Client } from 'pg';
+import { Client } from 'pg';
 import Router from 'express-promise-router';
 import 'babel-polyfill';
-import path from 'path';
 
 const app = express();
 const router = new Router();
 
-if (process.env.NODE_ENV !== 'production') {
-  let dotenv = require('dotenv');
-  dotenv.config();
-}
+// .env Variables will be passed in via docker-compose
+
+// if (process.env.NODE_ENV !== 'production') {
+//   const dotenv = require('dotenv');
+//   dotenv.config();
+// }
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('./'));
-  app.get('/', function(req, res) {
+  app.get('/', (req, res) => {
     res.sendFile('./index.html');
   });
 }
@@ -27,10 +28,14 @@ const client = new Client({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASS,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
 });
 
 client.connect();
+
+if (process.env.NODE_ENV === 'development') {
+  seedData();
+}
 
 router.get('/api/:fn/:ln', async (req, res) => {
   const query = `SELECT * FROM ${process.env.DB_TABLE} WHERE first_name ilike $1::text and last_name ilike $2::text`;
@@ -43,3 +48,32 @@ app.use(router);
 app.listen(app.get('port'), () => {
   console.log(`Find the server at: http://localhost:${app.get('port')}/`);
 });
+
+function seedData() {
+  const dropTableQuery = {
+    text: 'DROP TABLE users',
+  };
+
+  const createTableQuery = {
+    text: `CREATE TABLE users(
+      id INT PRIMARY KEY NOT NULL,
+      name TEXT,
+      email TEXT
+   )`,
+  };
+
+  const insertUserQuery = {
+    text: 'INSERT INTO users(id, name, email) VALUES($1, $2, $3)',
+    values: [1, 'blake', 'b@g.com'],
+  };
+
+  const selectUsersQuery = {
+    text: 'SELECT * FROM users',
+  };
+
+  client.query(dropTableQuery)
+    .then(() => client.query(createTableQuery))
+    .then(() => client.query(insertUserQuery))
+    .then(() => client.query(selectUsersQuery))
+    .then(res => console.log(res));
+}
